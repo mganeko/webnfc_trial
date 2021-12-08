dd182565632763638f9c7dd45e77ed8ed6c86e4a
mganeko <masashi.g@gmail.com>
2021-12-08 21:05:33 +0900

    multi read

--- script.js
+++ script.js
@@ -35,26 +35,69 @@ readButton.addEventListener("click", async () => {
 
     reader.addEventListener("error", () => {
       console.log("Error");
+      readLog.textContent = "scan error";
     });
 
     reader.addEventListener("reading", ({ message, serialNumber }) => {
       console.log(`> Serial Number: ${serialNumber}`);
       console.log(message);
-      const messageString = JSON.stringify(message);
-      //const messageString = "message records=" + message.records.length;
+      //const messageString = JSON.stringify(message);
+      const messageString = "records count=" + message.records.length;
       readLog.textContent = messageString;
       const serialString = `Serial Number: ${serialNumber}`
       readSerial.textContent = serialString;
 
-      const record = message.records[0];
-      const { data, encoding, recordType } = record;
-      if (recordType === "text") {
-        const textDecoder = new TextDecoder(encoding);
-        const text = textDecoder.decode(data);
-        readText.textContent = text;
+      let str;
+      for (const record of message.records) {
+        str += (decodeRecord(record) + "<br />\n");
       }
+      readText.textContent = str;
     });
   } catch (error) {
     readLog.textContent = error;
   }
 });
+
+function decodeRecord(record) {
+  const { data, encoding, recordType } = record;
+  if (recordType === "text") {
+    const textDecoder = new TextDecoder(encoding);
+    const text = textDecoder.decode(data);
+    return text;
+  }
+
+  switch (recordType) {
+    case "text":
+      const textDecoder = new TextDecoder(encoding);
+      const text = "text: " + textDecoder.decode(data);
+      return text;
+
+    case "url":
+      const decoder = new TextDecoder();
+      const url = "url: " + decoder.decode(data);
+      return url;
+
+    case "mime":
+      if (record.mediaType === "application/json") {
+        const decoder = new TextDecoder();
+        const json = "JSON: " + decoder.decode(data);
+        return json;
+      }
+      else if (record.mediaType.startsWith("image/")) {
+        return "image";
+
+        // const blob = new Blob([record.data], { type: record.mediaType });
+
+        // const img = document.createElement("img");
+        // img.src = URL.createObjectURL(blob);
+        // img.onload = () => window.URL.revokeObjectURL(this.src);
+
+        // document.body.appendChild(img);
+      }
+      else {
+        return "Media not handled";
+      }
+    default:
+      return "Record not handled";
+  }
+}
